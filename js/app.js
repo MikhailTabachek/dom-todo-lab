6a6e4909d9a4c2661093bc89b8ec8b51e69fed8b
Mikhail <mishanyaveselyi@gmail.com>
2022-02-03 00:13:50 -0500

    Make button clickable

--- js/app.js
+++ js/app.js
@@ -1,2 +1,30 @@
 console.log("test!")
 
+
+//created variable that stores input value
+function getText(){
+  let inputText = document.getElementById("input").value;
+  // document.querySelector("#to-do-list").textContent = inputText
+  let ul = document.getElementById("to-do-list")
+  let li = document.createElement("li")
+  li.appendChild(document.createTextNode(inputText))
+  ul.appendChild(li)
+  document.getElementById('input').value='';
+  
+  // document.querySelector("to-do-list").onclick.createElement(li)
+  
+}
+
+function removeText(){
+  let ul = document.getElementById("to-do-list")
+  let li = document.removeElement("li")
+  li.appendChild(document.removeTextNode(inputText))
+  ul.appendChild(li)
+}
+
+function removeTextFromInput(){
+
+}
+
+
+
